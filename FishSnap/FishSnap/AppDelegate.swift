df461cd9c74a59a9b8aa90128af64e60123803c2
Dulio Denis <dulio.denis@yahoo.com>
2018-02-10 14:02:47 -0500

    Initial App commit

---
+++ FishSnap/FishSnap/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  FishSnap
+//
+//  Created by Dulio Denis on 2/10/18.
+//  Copyright © 2018 ddApps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ FishSnap/FishSnap/Controllers/CameraController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraController.swift
+//  FishSnap
+//
+//  Created by Dulio Denis on 2/10/18.
+//  Copyright © 2018 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class CameraController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
